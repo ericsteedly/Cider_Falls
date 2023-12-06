bd3753fe05c580c8b8fafeb213789e0069cc44be
Eric Steedly <ericsteedly@gmail.com>
2023-12-06 15:02:05 -0600

    set up database and modules

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+//create main html in main js, where we can call functions to import the rest of the html
+
+const mainContainer = document.querySelector("#container")
+
+const applicationHTML = 
+`
+`
