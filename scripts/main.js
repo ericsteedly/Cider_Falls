c9525fd8cbca7d0826d138369053f3f3673be3fc
Kimberly Summers <kasummers16@gmail.com>
2023-12-07 13:53:42 -0600

    added guest function

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 //create main html in main js, where we can call functions to import the rest of the html
+import { GuestsList } from "./guests.js"
 
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = 
+`<aside class = "guests">
+<h2>Guest List</h2>
+${GuestsList()}
+</aside>
 `
-`
+
+mainContainer.innerHTML = applicationHTML
--- scripts/services.js
+++ scripts/services.js
@@ -1,9 +1,26 @@
 //look at each park Area and see what services are offered there
 //look at each guest and see where they are in the park
 
+//iterate through parkAreas, for each park look through all areaServices and compare serviceId
+//return park name and servicesNames
 import { getServices, getParkServices, getParkArea } from "./database.js";
 
 
+const parkAreas = getParkArea() 
+    
+const ParkAreaName = () => {
+
+    for (const park of parkAreas) {
+        
+    }
+
+}
+
+
+
 const ServiceList = () => { 
-    let html = "<section class='service'>"
+    let servicesHTML = "<section class='service'>"
+
+
+    
 }
