e481d868d47526cc8b3eaab020bc473008d46889
Eric Steedly <ericsteedly@gmail.com>
2023-12-08 13:34:06 -0600

    added services/location cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,18 @@
 //create main html in main js, where we can call functions to import the rest of the html
 import { GuestsList } from "./guests.js"
+import { ParkAreaName } from "./services.js"
 
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = 
-`<aside class = "guests">
+`<article id="areas_main">
+${ParkAreaName()}
+</article>
+<aside class = "guests">
 <h2>Guest List</h2>
 ${GuestsList()}
 </aside>
 `
+
 
 mainContainer.innerHTML = applicationHTML
